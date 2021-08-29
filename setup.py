89e1d856b220126209c052cce1540ea188ab6a72
Daksh Srivastava <daksh.srivastava.10@gmail.com>
2021-08-29 16:45:09 -0400

    structure and moveAnalyze.py adapted for module purposes

---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(name="position_analyze", packages=find_packages())
+
---
+++ tests/moveAnalyze_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import position_analyze
+import position_analyze.moveAnalyze
+import pytest
+
+print(__name__)
+
