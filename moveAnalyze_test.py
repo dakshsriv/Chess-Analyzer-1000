f41b45bc1f05c4b09256dae2bce2d0b4453c9c5a
Daksh Srivastava <daksh.srivastava.10@gmail.com>
2021-09-05 15:56:27 -0400

    Some errors fixed, inconsistency to be compensated for

--- moveAnalyze_test.py
+++ moveAnalyze_test.py
@@ -8,7 +8,7 @@ from stockfish import Stockfish
 
 def gb(fen, move):
     stockfish = Stockfish("Engines/stockfish_14_linux_x64/stockfish_14_x64")
-    stockfish = Stockfish(parameters={"Threads": 2, "Minimum Thinking Time": 30, "Ponder": True})
+    stockfish = Stockfish(parameters={"Threads": 3, "Minimum Thinking Time": 30, "Ponder": True})
     stockfish.set_fen_position(fen)
     eval = stockfish.get_evaluation()
     cp = eval["value"]
@@ -34,7 +34,7 @@ def gb(fen, move):
 
     return eval, bls, eval2, bls2
     
-def test_bestmove():
+def test_bestmove1():
     move = "c5f2"
     fen = "r1bqk2r/pppp1Npp/2n2n2/2b1p3/2B1P3/8/PPPP1PPP/RNBQK2R b KQkq - 0 5"
     lz = position_analyze.moveAnalyze.getStats(fen, move)
@@ -46,6 +46,8 @@ def test_bestmove():
     pprint.pprint(la)
     #assert ["Best move", eval, bls, eval2, bls2] == lz
     assert la == lz
+
+def test_bestmove2():
     #fen2 = "r1b1k2r/pp3ppp/2p5/3p4/3P4/2n5/PPP3PP/R1B1KB2 w kq - 0 17"
     move = "c6d4"
     fen = "r3k2r/ppp2Npp/2n5/3Bpb2/5q2/8/PPPPK1PP/RNBQ3R b kq - 2 11"
@@ -60,6 +62,17 @@ def test_bestmove():
     assert la == lz
 
 def test_inaccuracy():
-    pass
+    move = "f8b4"
+    fen = "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 2 3"
+    lz = position_analyze.moveAnalyze.getStats(fen, move)
+    lz[2] = set(lz[2])
+    lz[4] = set(lz[4])
+    pprint.pprint(lz)
+    eval, bls, eval2, bls2 = gb(fen, move)
+    la = ["Inaccuracy", eval, bls, eval2, bls2]
+    pprint.pprint(la)
+    #assert ["Best move", eval, bls, eval2, bls2] == lz
+    assert la == lz
 
-test_bestmove()
+#test_bestmove1()
+#test_bestmove2()
--- position_analyze/moveAnalyze.py
+++ position_analyze/moveAnalyze.py
@@ -1,12 +1,9 @@
 from stockfish import Stockfish
 import sys
 
-stockfish = Stockfish("/home/daksh/Projects/Chess_Analyzer/Engines/stockfish_14_linux_x64/stockfish_14_x64")
-stockfish = Stockfish(parameters={"Threads": 2, "Minimum Thinking Time": 30, "Ponder": True})
-
 #stockfish.set_fen_position("rnbqkbnr/pppp1ppp/8/4p3/5P2/8/PPPPP1PP/RNBQKBNR w KQkq - 0 1")
 #stockfish.set_fen_position("r1bqkbnr/pppp1ppp/2n5/4p2Q/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 3 3")
-stockfish.set_fen_position("r1bBk2r/ppp2ppp/2p5/8/4n3/3P4/PPP1KbPP/RN1Q1B1R b kq - 1 8")
+#stockfish.set_fen_position("r1bBk2r/ppp2ppp/2p5/8/4n3/3P4/PPP1KbPP/RN1Q1B1R b kq - 1 8")
 
 #move = "g2g4"
 #move = "g8f6"
@@ -61,10 +58,9 @@ def gof(stockfish):
 
 def getStats(fen, move):
     stockfish = Stockfish("/home/daksh/Projects/Chess_Analyzer/Engines/stockfish_14_linux_x64/stockfish_14_x64")
-    stockfish = Stockfish(parameters={"Threads": 2, "Minimum Thinking Time": 30, "Ponder": True})
+    stockfish = Stockfish(parameters={"Threads": 3, "Minimum Thinking Time": 30, "Ponder": True})
     stockfish.set_fen_position(fen)
     stockfish, eval, bestmoves = gof(stockfish)
-    global moveQuality
     moveQuality = str()
     bls = [x["Move"] for x in bestmoves]
     checkmates = [x["Mate"] for x in bestmoves]
@@ -72,34 +68,31 @@ def getStats(fen, move):
         if checkmates[x] == None:
             checkmates[x] = False
     isMate = checkmates[0] or checkmates[1] or checkmates[2]
-    print(isMate)
     stockfish.make_moves_from_current_position([move])
     stockfish, eval2, bestmoves2 = gof(stockfish)
     bls2 = [x["Move"] for x in bestmoves2]
     isMate2 = False
     if eval2 <= -10000 or eval2 >= 10000:
         isMate2 = True
-    print(isMate, isMate2, eval)
     if not isMate and isMate2:
         #print("Game-losing blunder! This move blunders forced mate or mate-in-one.")
         moveQuality = "Blunder"
     if not isMate2 and isMate:
         #print("Missed Win! You missed a move that could've won the game.")
         moveQuality = "Missed Win"
+    print(f"eval is :{eval}, eval2 is: {eval2}")
     if not isMate and not isMate2:
-        evalDiff = eval2 - eval
-        print(f"eval2 is {eval2}, eval is {eval}")
-        print(evalDiff)
-        if evalDiff <= -3:
+        eDbm = -1*abs(eval2 - eval)
+        if eDbm <= -3:
             #print("That move was a blunder! It was a very bad move.")
             moveQuality = "Blunder"
-        elif evalDiff <= -2:
+        elif eDbm <= -2:
             #print("That move was a big mistake. It was a fairly bad move.")
             moveQuality = "Big Mistake"
-        elif evalDiff <= -1:
+        elif eDbm <= -1:
             print("That move was a small mistake. It was a mildly bad move.")
             moveQuality = "Small Mistake"
-        elif evalDiff <= 0.5:
+        elif eDbm <= 0.5:
             moveQuality = "Inaccuracy"
 
         if move == bls[0]:
@@ -112,10 +105,10 @@ def getStats(fen, move):
             #print(f"Best move! {bls[0]} and {bls[1]} are alternatives.")
             moveQuality = "Best move"
         return [moveQuality, eval, bls, eval2, bls2]
-        
-
 try:
     if __name__ == "__main__" and sys.argv[1] == "-d":
+        stockfish = Stockfish("Engines/stockfish_14_linux_x64/stockfish_14_x64")
+        stockfish = Stockfish(parameters={"Threads": 3, "Minimum Thinking Time": 30, "Ponder": True})
         fen = sys.argv[2] + " " + sys.argv[3] + " " + sys.argv[4] + " " + sys.argv[5] + " " + sys.argv[6] + " " + sys.argv[7]
         stockfish.set_fen_position(fen)
         move = sys.argv[8]
