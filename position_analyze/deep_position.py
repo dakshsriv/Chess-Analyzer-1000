6688cfa76349df29590b2730dc1a6ef7f936b50e
Daksh Srivastava <daksh.srivastava.10@gmail.com>
2021-09-26 20:15:25 -0400

    MVP in progress. First base gui done

--- position_analyze/deep_position.py
+++ position_analyze/deep_position.py
@@ -7,4 +7,4 @@ def anylz(fen, move):
     stockfish, eval, bls, _, _ = moveAnalyze.getStats(fen, move)
     print(f"In the current position, the best move is {bls[0]}. It: ")
     depthString1 = depthString(stockfish)
-    
+    stockfish.set_fen_position("r1bqk2r/pppp1Npp/2n2n2/2b1p3/2B1P3/8/PPPP1PPP/RNBQK2R b KQkq - 0 5")
--- position_analyze/depthString.py
+++ position_analyze/depthString.py
@@ -1,4 +1,4 @@
-import stockfish, chess
+import stockfish, chess, pprint
 
 def depthString(engine):
     depth_string = list()
@@ -9,11 +9,14 @@ def depthString(engine):
         engine.make_moves_from_current_position([move])
         topmoves = engine.get_top_moves(5)
         lineSum = 0
-        for x in range(0, 5):
-            lineSum = lineSum + (topmoves[x]["Centipawn"])*(2**(-1*x))
-            print(f"LineSum is : {lineSum} {topmoves[x]["Centipawn"]} ")
+        for x in range(0, 3):
+            lz = (topmoves[x]["Centipawn"])*(2**(-1*x))*0.01
+            print("move is {} lz is {}".format(move, lz))
+            lineSum = lineSum + (topmoves[x]["Centipawn"])*(2**(-1*x))*0.01
         lineSums.append(lineSum)
     return depth_string, lineSums
 
 stockfish_instance = stockfish.Stockfish("Engines/stockfish_14_linux_x64/stockfish_14_x64")
-print(depthString(stockfish_instance))
+stockfish_instance = stockfish.Stockfish(parameters={"Threads": 3, "Minimum Thinking Time": 100, "Ponder": True})
+stockfish_instance.set_fen_position("r1bqk2r/pppp1Npp/2n2n2/2b1p3/2B1P3/8/PPPP1PPP/RNBQK2R b KQkq - 0 5")
+pprint.pprint(depthString(stockfish_instance))
